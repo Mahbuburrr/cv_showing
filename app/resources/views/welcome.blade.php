9a0f519b0233cb35d698e74947c4d7cb683c41b6
Mahbuburrr <mahbuburcse191@gmail.com>
2023-01-23 23:19:05 +0600

    cv

---
+++ app/resources/views/welcome.blade.php
@@ -0,0 +1,162 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="{{asset('css/style.css')}}">
+    <title>Document</title>
+</head>
+<body>
+    
+    <div class="container">
+        <div class="row">
+            <div class="col-6">
+                <h4>Curriculum vita of </h4>
+            <h4>MD. Mahbubur</h4>
+            <h4>Rahman</h4>
+
+            <p>Address: House no: 921/2 , Road no: 04 <br>Mirpur, Dhaka-1216 <br>Cell- no: 01609-558069<br>                                                                             
+                e-mail: mahbuburcs191@gmail.com <br> Github: https://github.com/Mahbuburrr                                                                                                                                             
+                </p>
+                
+            </div>
+            <div class="col-6 pt-5 ">
+                <div class="image mr-50">
+                    <img style="height: 180px; width: 180px;" src="images/mahbubur.PNG" alt="images">
+                </div>
+            </div>
+                
+        </div><br><br>
+
+        <!-- =============second div  objective -->
+
+        <div class="objective pt-10">
+           
+            <h5 class="bg">Objective:</h5>
+            <p>I have sound knowledge of PHP and Laravel framework. I want to apply my knowledge and skills to help 
+                <br>achieve the business goal of the company.</p>
+         
+
+        </div><br><br>
+
+         <!-- =============Academic Qualifications======= -->
+
+         <div class="academic_quli pb-10">
+            <h5 class="bg">Academic Qualification:</h5>
+
+        <table border="1px" class="w-100 ">
+
+            <tr>
+                <th>Exam Title</th>
+                <th>Group</th>
+                <th>Institute</th>
+                <th>Result</th>
+                <th>Pass.
+                    Year
+                    </th>
+                <th>Duration</th>
+                <th>Board/University</th>
+
+            </tr>
+
+            <tr>
+                <td>B.Sc.
+                    Engineering
+                    </td>
+                <td>Compur Science & Engineering</td>
+                <td>Green University of Bangladesh</td>
+                <td>Studing final semister</td>
+                <td></td>
+                <td>4 Years</td>
+                <td>Green University of Bangladesh.</td>
+            </tr>
+            <tr>
+
+                <td>H.S.C</td>
+                <td>Science</td>
+                <td>Govt.Mounsur Ali College</td>
+                <td>CGPA: 3.83
+                    out of 5
+                    </td>
+                <td>2017</td>
+                <td>2years</td>
+                <td>Rajshahi</td>
+            </tr>
+            <tr>
+                <td>S.S.C</td>
+                <td>Science</td>
+                <td>Alampur N,M
+                    High School
+                    </td>
+                <td>GPA:
+                    4.61
+                    out of 5
+                    </td>
+                <td>2015</td>
+                <td>-</td>
+                <td>Rajshahi.</td>
+            </tr>
+        </table>
+         </div><br><br>
+
+<!-- ==============achievement part==================== -->
+         <div class="achievement ">
+
+            <h5 class="bg pt-10">Achievement:</h5>
+
+            <p>I am a web developer. I am specialized with PHP and Laravel frameworkPostgreSQL, MySQL, jQuery,<br> javascript, ajax, bootstrap, CSS, HTML. I am efficient in writing web service scripts. I use github and <br>gitlab for version control.</p>
+         </div><br><br>
+
+
+         <!-- Project sector=============== -->
+
+         <div class="project">
+            <h5 class="bg pt-10">Projects:</h5>
+            <h3>1.Ecommerce</h3>
+            <p class="t"> A customer & admin service</p>
+            <p class="t"> <span>Platfrom:</span> Laravel</p>
+            <p class="t"><span>My Contribution:</span> Admin panel bug fixing and feature adjustment.</p>
+
+            <h3>2.Hospital Management System</h3>
+            
+            <p class="t"> <span>Platfrom:</span> Laravel</p>
+            <p class="t"><span>My Contribution:</span> Admin panel bug fixing and feature adjustment.</p>
+         </div><br><br>
+
+         <!-- =============personal details===== -->
+         <div class="personal_details">
+
+            <h5 class="bg pt-10"> personal Details:</h5>
+            <p>Fathers Name  <span2 class="pace">:Md.Shajahan Ali</span2> </p>
+            <p>Mother's Name  <span2 class="pace1">: Most. Mazeda Khatun</span2> </p>
+            <p>Date of Birth  <span2 class="pace2">: 02-02-2000</span2> </p>
+            <p>Gender  <span2 class="pace3">:Male</span2> </p>
+            <p>Marital Status  <span2 class="pace4">:Single</span2> </p>
+            <p>Nationality  <span2 class="pace5">:Bangladeshi</span2> </p>
+            <p>National Id no   <span2 class="pace6">:7364380936</span2> </p>
+            <p>Religion  <span2 class="pace7">:Islam</span2> </p>
+            <p>Permanent Address  <span2 class="pace8">:Vill-Alampur, Post-Kazipur, Upazila-Kazipur, District-Sirajgonj. </span2> </p>
+            <p>Height  <span2 class="pace9">:5’-4”</span2> </p>
+            <p>Weight  <span2 class="pace10">:56Kg</span2> </p>
+            <p>Blood group  <span2 class="pace11">:O+</span2> </p>
+            <p>Hobbies  <span2 class="pace12">:Programming,Travelling</span2> </p>
+            <p>Current location  <span2 class="pace13">:Dhaka</span2> </p>
+
+         </div><br><br>
+         <div class="reference">
+
+            <h5 class="bg pt-10">  Reference (s):</h5>
+
+         </div> <br><br>
+
+         <div class="singnature ">
+            <p1>Signature()</p1>
+         </div><br><br>
+
+
+    </div>
+</body>
+</html>
